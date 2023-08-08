1459839fecd4282b6d4bdb1665e3db43ddb594e6
Fallen-dev <rishav.m.19.02@gmail.com>
2023-08-08 13:09:11 +0530

    Fully setup

--- install.sh
+++ install.sh
@@ -2,19 +2,43 @@
 
 apps="bspwm sxhkd dunst polybar rofi picom"
 
-copyto() {
-  echo cp $1 ~/.config/$1 -r
+copy() {
+  cp $1 ~/.config/$1 -r
 }
 
-[ -d ~/.config ] || mkdir ~/.config
+[ -d ~/.config ] || mkdir -v ~/.config
 
-for app in $apps; do copyto $app; done
+for app in $apps; do copy $app; done
 
-[ -f ~/.xinitrc ] && sed 's/exec .*/exec bspwm/' test -i || echo exec bspwm > ~/.xinitrc
+echo Dotfiles copied to config dir
 
+[ -f ~/.xinitrc ] && cp ~/.xinitrc ~/.xinitrc.bak
+
+echo '# USEFUL ENV VARS
+export XDG_CURRENT_DESKTOP="bspwm"
+export XDG_SESSION_DESKTOP="bspwm"
+export WINDOW_MANAGER="bspwm"
+export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share/:/usr/share}"
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+
+dbus-update-activation-environment --systemd --all
+xsetroot -cursor_name left_ptr
+
+exec bspwm
+' > ~/.xinitrc
+
+echo Added Xinit file, use \`startx\` command from the login shell
+
+sleep 0.3s
+
+echo Installing fonts...
 # install fonts
 mkdir -pv ~/.local/share/fonts/
 cp fonts/*.ttf ~/.local/share/fonts/
 fc-cache -fv
 
-echo "DONE! Logout and login using the command 'startx'"
+[ -d ~/Pictures ] || mkdir -v ~/Pictures
+cp wall.jpg ~/Pictures
+echo The wallpaper is placed at ~/Pictures dir
+
+echo Setup complete!!!
